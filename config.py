47b45a764f04645f6911578ec789a605b93927d7
aquib116 <rockygaming85400@gmail.com>
2025-03-24 22:33:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,12 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from os import environ 
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002376176943').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 class Config:
     API_ID = int(environ.get("API_ID", ""))
