b2e68b1793886e212a8b28d74526b1d5b7e688eb
aquib116 <rockygaming85400@gmail.com>
2025-03-24 23:55:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@
 import re
 from os import environ
 
-id_pattern = re.compile(r'^.\d+$')
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828,)
 
 class Config:
